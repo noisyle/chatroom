60d5d73a8f28e9489858e42bc3a7723a4c0cdcdd
noisyle <anyone.1984@gmail.com>
2016-03-29 11:03:20 +0800

    first commit

---
+++ src/main/java/com/noisyle/chatroom/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.noisyle.chatroom.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration 
+@ComponentScan("com.noisyle.chatroom.controller") 
+@EnableWebMvc
+@Import({ WebSocketConfig.class })
+public class WebConfig extends WebMvcConfigurerAdapter {
+	@Bean  
+    public UrlBasedViewResolver setupViewResolver() {  
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();  
+        resolver.setPrefix("/jsp/");  
+        resolver.setSuffix(".jsp");  
+        resolver.setViewClass(JstlView.class);  
+        return resolver;  
+    }
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+}
---
+++ src/main/java/com/noisyle/chatroom/controller/ChatRoomController.java
@@ -0,0 +1,28 @@
+package com.noisyle.chatroom.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.noisyle.chatroom.vo.ChatMessage;
+
+@Controller
+public class ChatRoomController {
+	private static Logger logger = LoggerFactory.getLogger(ChatRoomController.class);
+
+	@RequestMapping("/")
+	public String index() {
+		return "chat";
+	}
+
+	@MessageMapping("/hello")
+	@SendTo("/topic/hello")
+	public ChatMessage hello(ChatMessage msg) throws Exception {
+		logger.info(msg.getNickname() + " say: " + msg.getMsg());
+		return msg;
+	}
+
+}
